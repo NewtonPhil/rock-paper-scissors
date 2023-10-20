fd97f88172a143bee5e1c352c15057627523d337
Phil Newton <newtonphilipjames@gmail.com>
2023-10-20 22:52:33 +0100

    Added pseudocode for easier understanding

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+// 1. const computerSelection = store a random selection from three options
+//    1.1 const options = array [rock, paper, scissors]
 const options = ["rock", "paper", "scissors"]; 
-let playerSelection = prompt("Enter Rock, Paper, or Scissors").toLowerCase;
 
+//    1.2 use a function to pick the random result from const options
 function getComputerChoice() {
     const computerSelection = options[Math.floor(Math.random() * options.length)];
+
+//    1.3 return the random pick from const options to computerSelection to be used elsewhere    
     return computerSelection;                        
 }
 
+
+
+// 2. let playerSelection = store players selection from rock, paper, or scissors
+//    2.1 Prompt player to input rock, paper, or scissors and store in playerSelection
+//        2.1.1 Make the input case-insenstive with .toLowerCase
+let playerSelection = prompt("Enter Rock, Paper, or Scissors").toLowerCase;
+
+// 2.2 If the player inputs valid selection, return it to playerSelection
+    if (playerSelection === "rock" || "paper" || "scissors") {
+        return playerSelection;
+//    2.3 If an invalid input, send player a message saying so        
+    } else {
+        alert("Invalid input");
+    }
+
+
+
+
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        console.log("Fucking get in!")
-    } else {console.log("wtf")
+    
 }
-}
+
 
     
 
