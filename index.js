a24bc372b00eb621375592478da7d02564a5992e
Phil Newton <newtonphilipjames@gmail.com>
2023-10-25 21:48:31 +0100

    removed game function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let computerScore = 0;
+let playerScore = 0;
+
+
 const options = ["rock", "paper", "scissors"]; 
 
 let computerSelection = getComputerChoice();
@@ -35,27 +39,33 @@ function playRound(computerSelection, playerSelection) {
 
     else if (computerSelection === "rock") {
         if (playerSelection === "paper") {
+            playerScore++;
             return "You win! Computer chose rock!";
         }
         else {
+            computerScore++;
             return "You lose! Computer chose rock!";
         }
     }
 
     else if (computerSelection === "paper") {
         if (playerSelection === "rock") {
+            computerScore++;
             return "You lose! Computer chose paper!";
         }
         else {
+            playerScore++;
             return "You win! Computer chose paper!";
         }
     }
     
     else if (computerSelection === "scissors") {
         if (playerSelection === "paper") {
+            computerScore++;
             return "You lose! Computer chose scissors!";
         }
         else {
+            playerScore++;
             return "You win! Computer chose scissors!";
         }
     }
@@ -63,6 +73,8 @@ function playRound(computerSelection, playerSelection) {
 }
 
 console.log(playRound(computerSelection, playerSelection));
+console.log(playerScore);
+console.log(computerScore);
 
     
 
